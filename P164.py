085de42832a06cb12b994e780bfaaaac69eb19aa
aditi419 <76972940+aditi419@users.noreply.github.com>
2021-10-07 19:59:49 -0400

    Add files via upload

---
+++ P164.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import filedialog
+
+root = Tk()
+root.title('P164')
+root.geometry('500x500')
+root.configure(background='black')
+
+img_label = Label(root,bg='white',highlightthickness=5)
+img_label.place(relx=0.5,rely=0.2,anchor=CENTER)
+
+def stuff():
+    img_path = filedialog.askopenfilename(title='Open Text File',filetypes=[('Image Files','*.jpg;*.gif;*.jpg;;*.png;*txt')])
+    print(img_path)
+
+img_e = ImageTk.PhotoImage(Image.open(img_path))
+
+img_label['image'] = img_e
+img_e.close()
+
+btn = Button(root,text='Open Image',command=stuff,font=('courier',18,'bold'))
